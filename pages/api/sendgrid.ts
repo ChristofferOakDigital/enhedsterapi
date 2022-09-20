4a4d2c37541983e09d8db1280efceb992597b0cc
Christoffer Andersen <christoffer.andersen@oakdigital.dk>
2022-09-20 09:50:36 +0200

    adding sendgrid

---
+++ pages/api/sendgrid.ts
@@ -0,0 +1,24 @@
+import sendgrid from '@sendgrid/mail';
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY || '');
+
+async function sendEmail(req: any, res: any) {
+    try {
+        // console.log("REQ.BODY", req.body);
+        await sendgrid.send({
+            to: 'christoffer.t.andersen@gmail.com', // Your email where you'll receive emails
+            from: 'christoffer.t.andersen@gmail.com', // your website email address here
+            subject: `${req.body.subject}`,
+            html: `<div>You've got a mail</div>`,
+        });
+    } catch (error) {
+        // console.log(error);
+        return res
+            .status(error.statusCode || 500)
+            .json({ error: error.message });
+    }
+
+    return res.status(200).json({ error: '' });
+}
+
+export default sendEmail;
